7e32467ba3223dadd1e31a11ab23159e769a2527
Brent Steffen <BESteffen82@gmail.com>
2022-06-24 16:51:17 -0400

    Add merge_sort method

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,2 +1,15 @@
-def merge_sort
+require 'pry-byebug'
+
+def merge_sort(array)
+	return array if array.length <= 1
+
+	midpoint = (array.length) / 2
+	left = array[0..midpoint - 1]
+	right = array[midpoint..array.length - 1]
+	merge(left,right)	
 end
+
+def merge(left,right)
+end
+
+merge_sort([9,16,3,5,69,8,4,0,11])
