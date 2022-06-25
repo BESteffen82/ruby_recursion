30d64517e29f8ff395b9826ad53c24fab1961f61
Brent Steffen <BESteffen82@gmail.com>
2022-06-25 11:13:07 -0400

    Final code

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,15 +1,26 @@
-require 'pry-byebug'
-
 def merge_sort(array)
-	return array if array.length <= 1
-
-	midpoint = (array.length) / 2
-	left = array[0..midpoint - 1]
-	right = array[midpoint..array.length - 1]
-	merge(left,right)	
+	if array.length < 2
+		array
+	else 
+		midpoint = (array.length / 2)
+		left = merge_sort(array[0..midpoint - 1])
+		right = merge_sort(array[midpoint..array.length - 1])
+		merge(left,right)
+	end	
 end
 
 def merge(left,right)
+	if left.empty?
+		right
+	elsif right.empty?
+		left
+	elsif left.first < right.first
+		[left.first] + merge(left[1..left.length], right)
+	else 
+		[right.first] + merge(left, right[1..right.length])
+	end
 end
 
-merge_sort([9,16,3,5,69,8,4,0,11])
+random_array = Array.new(11){rand(1..100)}
+p random_array
+p merge_sort(random_array)
